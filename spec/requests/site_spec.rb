80605a1f1effb5bb2a37f04c09ac96f3c4d3bf08
Andy Cox <acox@weather.com>
2014-02-25 16:02:15 -0500

    Make the specs green

--- spec/requests/site_spec.rb
+++ spec/requests/site_spec.rb
@@ -4,6 +4,6 @@ describe "visiting in a browser" do
   it "redirects to github.com" do
     get "/"
     expect(response).to be_redirect
-    expect(response.headers['Location']).to eq("https://github.com/tampopo/heroku-deploy")
+    expect(response.headers['Location']).to eq("https://github.com/atmos/heaven")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_SECRET_KEY_BASE"] ||= ('x' * 30)
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
@@ -16,6 +18,8 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
+
+  #config.backtrace_clean_patterns = []
 
   config.before do
     Resque.inline = true
