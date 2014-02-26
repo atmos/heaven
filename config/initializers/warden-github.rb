1dbd027c2b751a7f2a2123b9d5fd812b2fa3db9f
Corey Donohoe <atmos@atmos.org>
2014-02-25 21:36:22 -0800

    use read:org instead of user scope

--- config/initializers/warden-github.rb
+++ config/initializers/warden-github.rb
@@ -3,7 +3,7 @@ require 'warden/github/rails'
 Warden::GitHub::Rails.setup do |config|
   config.add_scope :user,  :client_id     => ENV['GITHUB_CLIENT_ID'],
                            :client_secret => ENV['GITHUB_CLIENT_SECRET'],
-                           :scope         => 'user'
+                           :scope         => ["read:org"]
 
   config.add_team :employees, ENV['GITHUB_TEAM_ID'] || '135235'
 
