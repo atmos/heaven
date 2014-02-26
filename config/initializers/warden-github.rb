dc234cfdcf98ff2fe42ba8a834859fad50dac9ea
Corey Donohoe <atmos@atmos.org>
2014-02-25 21:37:05 -0800

    easier oauth perms

--- config/initializers/warden-github.rb
+++ config/initializers/warden-github.rb
@@ -5,7 +5,7 @@ Warden::GitHub::Rails.setup do |config|
                            :client_secret => ENV['GITHUB_CLIENT_SECRET'],
                            :scope         => ["read:org"]
 
-  config.add_team :employees, ENV['GITHUB_TEAM_ID'] || '135235'
+  config.add_team :employees, ENV['GITHUB_TEAM_ID'] || '696075'
 
   config.default_scope = :user
 end
