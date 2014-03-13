049abc421780b3d0caacb6739a868bc58a0c657a
Corey Donohoe <atmos@atmos.org>
2014-03-12 17:54:35 -0700

    don't verify authenticity on github posts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+
   def create
     request.body.rewind
     data = request.body.read
