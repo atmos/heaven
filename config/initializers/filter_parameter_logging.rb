4625aefc9ce184fa99b5a5a35c932858dee79914
Corey Donohoe <atmos@atmos.org>
2014-03-12 17:40:32 -0700

    enumerate all the payload keys so logs aren't as noisy

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,1 +1,2 @@
-Rails.application.config.filter_parameters += [:password, :payload, :config]
+param_keys =  [:branches, :commit, :config, :context, :description, :id, :name, :password, :payload, :repository, :sender, :sha, :state, :target_url]
+Rails.application.config.filter_parameters += param_keys
