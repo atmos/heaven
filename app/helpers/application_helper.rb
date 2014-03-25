c1378c87b162618c26f32e640bfbc4972719dc36
Corey Donohoe <atmos@atmos.org>
2014-03-24 19:41:14 -0700

    make a class for ip validation to avoid unauth'd rate limiting on heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,2 @@
 module ApplicationHelper
-  def incoming_ip_valid?(ip)
-    return true if ["127.0.0.1", "0.0.0.0"].include?(ip) && Rails.env == "test"
-    hook_source_ips.any? { |block| IPAddr.new(block).include?(ip) }
-  end
-
-  private
-    VERIFIER_KEY = "hook-sources-#{Rails.env}"
-
-    def source_key
-      VERIFIER_KEY
-    end
-
-    def default_ttl
-      %w(staging production).include?(Rails.env) ? 60 : 2
-    end
-
-    def hook_source_ips
-      if addresses = Heaven.redis.get(source_key)
-        JSON.parse(addresses)
-      else
-        addresses = Octokit::Client.new.get("/meta").hooks
-        Heaven.redis.set(source_key, JSON.dump(addresses))
-        Heaven.redis.expire(source_key, default_ttl)
-        Rails.logger.info "Refreshed GitHub hook sources"
-        addresses
-      end
-    end
 end
