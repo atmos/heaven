586f556d85e8031b5985ec89089809e6bc65e5ec
Corey Donohoe <atmos@atmos.org>
2014-03-28 16:06:39 -0700

    delegate providers based on string name

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,24 @@
 module Provider
-  def self.from_payload(guid, payload, token)
-    Provider::Dpl.new(guid, payload, token)
+  def self.from(guid, payload, token)
+    klass = provider_class_from(payload)
+    klass.new(guid, payload, token)
+  end
+
+  def self.provider_class_for(payload)
+    case provider_name_for(payload)
+    when "heroku"
+      Provider::Heroku
+    else
+      Provider::Dpl
+    end
+  end
+
+  def self.provider_name_for(payload)
+    data = JSON.parse(payload)
+    if data && data['payload']
+     if custom_payload = data['payload']['config']
+       return custom_payload['provider']
+     end
+    end
   end
 end
