3a207b3c545366f0c88ae6e444a9d18084426628
Corey Donohoe <atmos@atmos.org>
2014-03-29 17:08:19 -0700

    get the deployments that possibly match for a repository

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -1,6 +1,12 @@
 class Deployment < ActiveRecord::Base
   validates_presence_of :name, :name_with_owner
 
+  def self.latest_for_name_with_owner(name_with_owner)
+    self.where(:name_with_owner => name_with_owner).
+      group("name,environment").
+      order("created_at desc")
+  end
+
   def payload
     @payload ||= JSON.parse(custom_payload)
   end
