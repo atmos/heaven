997b0c39d627ba56c0aed0d35fbe45ab8ee2ba3f
Corey Donohoe <atmos@atmos.org>
2014-04-07 13:33:37 -0700

    still not working, strangely

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,11 +1,8 @@
-require 'secure_headers'
 
 ::SecureHeaders::Configuration.configure do |config|
+  config.csp = { :default_src => "'none'" }
   config.hsts = {:max_age => 20.years.to_i, :include_subdomains => true}
-  config.x_frame_options = 'DENY'
   config.x_content_type_options = "nosniff"
+  config.x_frame_options = { :value => 'DENY' }
   config.x_xss_protection = {:value => 1, :mode => 'block'}
-  config.csp = {
-    :default_src => "'none'"
-  }
 end
--- spec/requests/site_spec.rb
+++ spec/requests/site_spec.rb
@@ -4,6 +4,18 @@ describe "visiting in a browser" do
   it "redirects to github.com" do
     get "/"
     expect(response).to be_redirect
+
+    headers = response.headers
+    expect(headers['Location']).to eq("https://github.com/atmos/heaven")
+    expect(headers['X-Frame-Options']).to eq("DENY")
+    expect(headers['X-XSS-Protection']).to eq("1; mode=block")
+    expect(headers['X-Content-Type-Options']).to eq("nosniff")
+  end
+
+  it "authenticates resque against github.com" do
+    pending
+    get "/resque"
+    expect(response).to be_redirect
     expect(response.headers['Location']).to eq("https://github.com/atmos/heaven")
   end
 end
