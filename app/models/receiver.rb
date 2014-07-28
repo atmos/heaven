75f2552875794530641bd18ec8a0b24313ce3bea
Corey Donohoe <atmos@atmos.org>
2014-07-28 14:00:55 -0700

    only deploy active repositories

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -10,7 +10,31 @@ class Receiver
   end
 
   def self.perform(event, guid, payload)
-    new(event, guid, payload).run!
+    receiver = new(event, guid, payload)
+    unless receiver.active_repository?
+      Rails.logger.info "Repository is not configured to deploy: #{receiver.full_name}"
+    else
+      receiver.run!
+    end
+  end
+
+  def data
+    @data ||= JSON.parse(payload)
+  end
+
+  def full_name
+    data['repository'] && data['repository']['full_name']
+  end
+
+  def active_repository?
+    if data['repository']
+      name  = data['repository']['name']
+      owner = data['repository']['owner']['login']
+      repository = Repository.find_or_create_by(name: name, owner: owner)
+      repository.active?
+    else
+      false
+    end
   end
 
   def run!
