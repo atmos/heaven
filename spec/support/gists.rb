0a28e75e2e91522cda01d377a22733616c65d86b
Corey Donohoe <atmos@atmos.org>
2014-09-04 14:51:41 -0700

    appease the rubocop

--- spec/support/gists.rb
+++ spec/support/gists.rb
@@ -1,12 +1,19 @@
 module Gists
   def stub_gists
-    stub_request(:post, "https://api.github.com/gists").
-      with(:body => "{\"files\":{\"stdout\":{\"content\":\"Deployment 721 pending\"}},\"public\":false,\"description\":\"Heaven number 721 for zero-fucks-hubot\"}",
-           :headers => { 'Authorization'=>'token <unknown>'}).
-      to_return(:status => 200, :body => double('id' => "cd520d99c3087f2d18b4"))
+    request_body = {
+      "files"    => {
+        "public" => false,
+        "stdout" => { "content" => "Deployment 721 pending" },
+        "description" => "Heaven number 721 for zero-fucks-hubot"
+      }
+    }.to_json
 
-    stub_request(:patch, "https://api.github.com/gists/cd520d99c3087f2d18b4").
-      to_return(:status => 200, :body => "", :headers => {})
+    stub_request(:post, "https://api.github.com/gists")
+      .with(:body => request_body, :headers => { "Authorization" => "token <unknown>" })
+      .to_return(:status => 200, :body => double("id" => "cd520d99c3087f2d18b4"))
+
+    stub_request(:patch, "https://api.github.com/gists/cd520d99c3087f2d18b4")
+      .to_return(:status => 200, :body => "", :headers => {})
   end
 
   ::RSpec.configure do |config|
--- spec/support/statuses.rb
+++ spec/support/statuses.rb
@@ -24,21 +24,35 @@ module Statuses
     end
   end
 
+  def deployment_url(path = "")
+    "https://api.github.com/repos/atmos/my-robot/deployments/721#{path}"
+  end
+
   def stub_deploy_statuses
-    stub_request(:get, "https://api.github.com/repos/atmos/my-robot/deployments/721").
-      to_return(:status => 200, :body => StubDeployment.new("atmos/my-robot", 721), :headers => {})
+    deployment_results = {
+      :body => StubDeployment.new("atmos/my-robot", 721),
+      :status => 200,
+      :headers => {}
+    }
+    stub_request(:get, deployment_url)
+      .to_return(deployment_results)
 
-    stub_request(:post, "https://api.github.com/repos/atmos/my-robot/deployments/721/statuses").
-      with(:body => "{\"target_url\":\"https://gist.github.com/cd520d99c3087f2d18b4\",\"description\":\"Deploying from Heaven v#{Heaven::VERSION}\",\"state\":\"pending\"}").
-      to_return(:status => 201, :body => {}, :headers => {})
+    extra_params = {
+      "target_url"  => "https://gist.github.com/cd520d99c3087f2d18b4",
+      "description" => "Deploying from Heaven v#{Heaven::VERSION}"
+    }
 
-    stub_request(:post, "https://api.github.com/repos/atmos/my-robot/deployments/721/statuses").
-      with(:body => "{\"target_url\":\"https://gist.github.com/cd520d99c3087f2d18b4\",\"description\":\"Deploying from Heaven v#{Heaven::VERSION}\",\"state\":\"failure\"}").
-      to_return(:status => 201, :body => {}, :headers => {})
+    stub_request(:post, deployment_url("/statuses"))
+      .with(:body => extra_params.merge("state" => "pending").to_json)
+      .to_return(:status => 201, :body => {}, :headers => {})
 
-    stub_request(:post, "https://api.github.com/repos/atmos/my-robot/deployments/721/statuses").
-      with(:body => "{\"target_url\":\"https://gist.github.com/cd520d99c3087f2d18b4\",\"description\":\"Deploying from Heaven v#{Heaven::VERSION}\",\"state\":\"success\"}").
-      to_return(:status => 201, :body => {}, :headers => {})
+    stub_request(:post, deployment_url("/statuses"))
+      .with(:body => extra_params.merge("state" => "failure").to_json)
+      .to_return(:status => 201, :body => {}, :headers => {})
+
+    stub_request(:post, deployment_url("/statuses"))
+      .with(:body => extra_params.merge("state" => "success").to_json)
+      .to_return(:status => 201, :body => {}, :headers => {})
   end
 
   ::RSpec.configure do |config|
