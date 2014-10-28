e990cc46a7dc2459a41540693b5f123d4550ab25
Devon Blandin <dblandin@gmail.com>
2014-10-28 15:32:11 -0500

    Move top-level heaven module configuration to lib/heaven.rb

--- lib/heaven.rb
+++ lib/heaven.rb
@@ -1,6 +1,39 @@
 # The top-level Heaven module
 module Heaven
+  REDIS_PREFIX = "heaven:#{Rails.env}"
+
+  class << self
+    attr_writer :testing, :redis
+
+    def testing?
+      @testing.present?
+    end
+
+    def redis
+      @redis ||= if ENV["REDIS_PROVIDER"]
+                   Redis.new(:url => ENV[ENV["REDIS_PROVIDER"]])
+                 elsif ENV["REDISCLOUD_URL"]
+                   Redis.new(:url => ENV["REDISCLOUD_URL"])
+                 elsif ENV["OPENREDIS_URL"]
+                   Redis.new(:url => ENV["OPENREDIS_URL"])
+                 elsif ENV["BOXEN_REDIS_URL"]
+                   Redis.new(:url => ENV["BOXEN_REDIS_URL"])
+                 else
+                   Redis.new
+                 end
+
+      @redis
+    end
+
+    def redis_reconnect!
+      @redis = nil
+      redis
+    end
+  end
 end
+
+# initialize early to ensure proper resque prefixes
+Heaven.redis
 
 require "heaven/version"
 require "heaven/jobs"
