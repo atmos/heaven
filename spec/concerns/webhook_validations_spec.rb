0f24dc7b0687de88322a8dab54837bd43cd5aada
Devon Blandin <dblandin@gmail.com>
2014-11-15 21:55:40 -0600

    Move GitHub source validation to separate file

--- spec/concerns/webhook_validations_spec.rb
+++ spec/concerns/webhook_validations_spec.rb
@@ -25,12 +25,4 @@ describe WebhookValidations do
     klass = WebhookValidationsTester.new("127.0.0.1")
     expect(klass).to_not be_valid_incoming_webhook_address
   end
-
-  context "verifies IPs" do
-    it "returns production" do
-      expect(WebhookValidations::Validator.new("127.0.0.1")).to_not be_valid
-      expect(WebhookValidations::Validator.new("192.30.252.41")).to be_valid
-      expect(WebhookValidations::Validator.new("192.30.252.46")).to be_valid
-    end
-  end
 end
---
+++ spec/validators/github_source_validator_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe GithubSourceValidator do
+  context "verifies IPs" do
+    it "returns production" do
+      expect(GithubSourceValidator.new("127.0.0.1")).to_not be_valid
+      expect(GithubSourceValidator.new("192.30.252.41")).to be_valid
+      expect(GithubSourceValidator.new("192.30.252.46")).to be_valid
+    end
+  end
+end
