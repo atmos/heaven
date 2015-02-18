8e1798fd7964b1c6bd45cbe700efcd5b24501c66
Devon Blandin <dblandin@gmail.com>
2015-02-18 13:07:02 -0600

    Update capistrano provider spec to use decoded fixture data

--- spec/lib/heaven/provider/capistrano_spec.rb
+++ spec/lib/heaven/provider/capistrano_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Heaven::Provider::Capistrano do
   include FixtureHelper
 
-  let(:deployment) { Heaven::Provider::Capistrano.new(SecureRandom.uuid, fixture_data("deployment-capistrano")) }
+  let(:deployment) { Heaven::Provider::Capistrano.new(SecureRandom.uuid, decoded_fixture_data("deployment-capistrano")) }
 
   it "finds deployment task" do
     expect(deployment.task).to eql "deploy:migrations"
