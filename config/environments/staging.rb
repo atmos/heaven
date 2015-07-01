a3a0d0fcebc4566f038e04a2c39f3f725d7ade17
Corey Donohoe <atmos@atmos.org>
2015-07-01 00:51:25 -0700

    silence warnings, fixes #155

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,9 +20,6 @@ Heaven::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
