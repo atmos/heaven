2d86646e904ea01bf72a774ba86a7ee87c6af9c9
amenasse <amenasse@gmail.com>
2015-09-10 16:15:46 +0900

    Add support for ansible vault
    Provide ansible-playbook with the vault password provided by the the
    ANSIBLE_VAULT_PASSWORD environment variable

--- lib/heaven/provider/waysact.rb
+++ lib/heaven/provider/waysact.rb
@@ -33,10 +33,18 @@ module Heaven
             "ansible_ssh_private_key_file=#{working_directory}/.ssh/id_rsa"
           ].join(" ")
 
+          ansible_vault_password = ENV["ANSIBLE_VAULT_PASSWORD"]
+          # ansible-vault doesn't have an argument to read the vault password
+          # directly insteas an executable which should output the password can
+          # be specified with the --vault-password-file argument.  The idea to
+          # use cat to read the password from stdin came from the ansible
+          # developers mailing list:
+          # https://groups.google.com/d/msg/ansible-devel/1vFc3y6Ogto/ne0xKq5pQXcJ
           deploy_string = ["ansible-playbook", "-i", ansible_hosts_file, ansible_site_file, "--tags", "deploy", "-u", "vagrant",
-                           "--verbose", "--extra-vars", ansible_extra_vars, "-vvvv"]
+                           "--verbose", "--extra-vars", ansible_extra_vars, "--extra-vars", "@vaults/vagrant_secrets.yml",
+                           "--vault-password-file=/bin/cat", "-vvvv"]
           log "Executing ansible: #{deploy_string.join(" ")}"
-          execute_and_log(deploy_string,  "ANSIBLE_HOST_KEY_CHECKING" => "false")
+          execute_and_log(deploy_string,  { "ANSIBLE_HOST_KEY_CHECKING" => "false" }, ansible_vault_password)
         end
       end
     end
